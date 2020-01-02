f064411db2456c00a9107bf401d360aa695594ea
qinzexing <qzexing@qq.com>
2020-01-02 19:56:10 +0800

    添加Eureka注册中心

---
+++ eureka-server/src/main/java/com/xing/eureka/EurekaServerApplication.java
@@ -0,0 +1,21 @@
+package com.xing.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Description:
+ * @Author: qzexing
+ * @Version: 1.0
+ * @Date: 2020/1/2 19:38
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class,args);
+    }
+
+}
