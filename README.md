5a1b556aa3108ca68de24213c1315a70ff825eb5
qinzexing <qzexing@qq.com>
2020-03-30 23:03:36 +0800

    项目架构初始搭建

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# micro-service
-微服务个人项目
+# xing-micro-service 个人微服务
+## 模块说明
+
+```
+xing-micro-service -------------- 父项目，公共依赖
+│  │─xing-commons --------------- 通用工具
+│  ├─xing-config ---------------- 配置中心
+│  ├─xing-gateway --------------- api网关
+│  ├─xing-job ------------------- 分布式任务调度
+│  ├─xing-monitor --------------- 监控
+│  ├─xing-register -------------- 注册中心
+│  ├─xing-security -------------- 认证中心
+│  ├─xing-service --------------- 业务模块
+```
+
--- xing-register/src/main/java/com/xing/register/RegisterApplication.java
+++ xing-register/src/main/java/com/xing/register/RegisterApplication.java
@@ -1,21 +1,21 @@
-package com.xing.eureka;
+package com.xing.register;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
- * @Description:
+ * @Description: Eureka注册中心
  * @Author: qzexing
  * @Version: 1.0
  * @Date: 2020/1/2 19:38
  */
 @EnableEurekaServer
 @SpringBootApplication
-public class EurekaServerApplication {
+public class RegisterApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class,args);
+        SpringApplication.run(RegisterApplication.class,args);
     }
 
 }
